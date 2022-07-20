7475d5edda5fc892f3d6f48407bc3af7000d7618
Patrick Burns <burns212@gmail.com>
2022-07-19 21:07:34 -0500

    done with simply books

--- pages/author/edit/[firebaseKey].js
+++ pages/author/edit/[firebaseKey].js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { getSingleAuthor } from '../../../api/authorData';
+import AuthorForm from '../../../components/forms/AuthorForm';
 
-export default function DynamicRoute2() {
+export default function EditAuthor() {
+  const [editItem, setEditItem] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
-  return (
-    <div>Dynamic Route 2 {firebaseKey}</div>
-  );
+
+  useEffect(() => {
+    getSingleAuthor(firebaseKey).then(setEditItem);
+  }, [firebaseKey]);
+
+  return (<AuthorForm obj={editItem} />);
 }
